667980fbec08e3660cdf00e88223a8e68bd2118d
dream-well <dreamwell506@outlook.com>
2023-12-06 15:11:23 +0000

    fix python to python3

--- scraping/utils.py
+++ scraping/utils.py
@@ -81,7 +81,7 @@ def update_repository(flag = 'patch'):
         new_major, new_minor, new_patch = new_version.split('.')
         major, minor, patch = scraping.__version__.split('.')
         if new_version != scraping.__version__:
-            os.system("python -e pip install -e .")
+            os.system("python3 -e pip install -e .")
         if major != new_major:
             return True
         if flag == 'major':
